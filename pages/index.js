7d60c619848934b79a4cf45278a4a8ae170859aa
Matt <hayaemsay@gmail.com>
2021-07-09 17:21:47 +1200

    Escape characters

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <div className={"m-box"}>
           <p className={"m m2"}>Typing...like this is...makes your message...difficult...to read</p>
           <p className={"m m1 delay1"}>Typing like this makes your message easy to read</p>
-          <p className={"m m1 delay2"}>If it's worth reading, it's worth your time to make it readable</p>
+          <p className={"m m1 delay2"}>If it&apos;s worth reading, it&apos;s worth your time to make it readable</p>
         </div>
       </main>
     </div>
